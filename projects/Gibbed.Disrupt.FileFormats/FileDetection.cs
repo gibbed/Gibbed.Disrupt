c1ef432171c4c245cc31877c255b93d328db413b
Rick Gibbed <rick@gibbed.us>
2020-11-02 23:47:28 -0600

    Improve file type detection.
    - Cleaner handling for magic detection in FileDetection.
    - Add a number of additional file types added to forks of the project.

--- projects/Gibbed.Disrupt.FileFormats/FileDetection.cs
+++ projects/Gibbed.Disrupt.FileFormats/FileDetection.cs
@@ -22,6 +22,7 @@
 
 using System;
 using System.Text;
+using Gibbed.IO;
 
 namespace Gibbed.Disrupt.FileFormats
 {
@@ -95,68 +96,36 @@ namespace Gibbed.Disrupt.FileFormats
                 return new Tuple<string, string>("gfx", "hkx");
             }
 
+            if (read >= 8 &&
+                guess[0] == 0xEF &&
+                guess[1] == 0xBB &&
+                guess[2] == 0xBF &&
+                guess[3] == '<' &&
+                guess[4] == '?' &&
+                guess[5] == 'x' &&
+                guess[6] == 'm' &&
+                guess[7] == 'l')
+            {
+                return new Tuple<string, string>("misc", "xml");
+            }
+
+            if (read >= 20)
+            {
+                uint magic = BitConverter.ToUInt32(guess, 16);
+                var result = DetectMagic(magic) ?? DetectMagic(magic.Swap());
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+
             if (read >= 4)
             {
                 uint magic = BitConverter.ToUInt32(guess, 0);
-
-                if (magic == 0x00584254 || magic == 0x54425800) // '\0XBT'
+                var result = DetectMagic(magic) ?? DetectMagic(magic.Swap());
+                if (result != null)
                 {
-                    return new Tuple<string, string>("gfx", "xbt");
-                }
-
-                if (magic == 0x4D455348) // 'MESH'
-                {
-                    return new Tuple<string, string>("gfx", "xbg");
-                }
-
-                if (magic == 0x54414D00 || magic == 0x004D4154) // '\0MAT'
-                {
-                    return new Tuple<string, string>("gfx", "material.bin");
-                }
-
-                if (magic == 0x53504B02) // 'SPK\2'
-                {
-                    return new Tuple<string, string>("sfx", "spk");
-                }
-
-                if (magic == 0x00032A02)
-                {
-                    return new Tuple<string, string>("sfx", "sbao");
-                }
-
-                if (magic == 0x4643626E) // 'FCbn'
-                {
-                    return new Tuple<string, string>("game", "fcb");
-                }
-
-                if (magic == 0x534E644E) // 'SNdN'
-                {
-                    return new Tuple<string, string>("game", "rnv");
-                }
-
-                if (magic == 0x474E5089) // 'PNG\x89'
-                {
-                    return new Tuple<string, string>("gfx", "png");
-                }
-
-                if (magic == 0x4D564D00)
-                {
-                    return new Tuple<string, string>("gfx", "MvN");
-                }
-
-                if (magic == 0x61754C1B)
-                {
-                    return new Tuple<string, string>("scripts", "luab");
-                }
-
-                if (magic == 0x47454F4D)
-                {
-                    return new Tuple<string, string>("gfx", "xbg");
-                }
-
-                if (magic == 0x00014C53)
-                {
-                    return new Tuple<string, string>("languages", "loc");
+                    return result;
                 }
             }
 
@@ -217,6 +186,11 @@ namespace Gibbed.Disrupt.FileFormats
                 return new Tuple<string, string>("misc", "Profile.xml");
             }
 
+            if (read >= 12 && text.StartsWith("<MinimapInfo") == true)
+            {
+                return new Tuple<string, string>("misc", "MinimapInfo.xml");
+            }
+
             if (read >= 12 && text.StartsWith("<stringtable") == true)
             {
                 return new Tuple<string, string>("text", "xml");
@@ -232,6 +206,63 @@ namespace Gibbed.Disrupt.FileFormats
                 return new Tuple<string, string>("game", "seq");
             }
 
+            if (read >= 8 && text.StartsWith("<Binary>") == true)
+            {
+                return new Tuple<string, string>("pilot", "pnm");
+            }
+
+            if (read >= 15 && text.StartsWith("SQLite format 3") == true)
+            {
+                return new Tuple<string, string>("db", "sqlite3");
+            }
+
+            if (read >= 2 && guess[0] == 'p' && guess[1] == 'A')
+            {
+                return new Tuple<string, string>("animations", "dpax");
+            }
+
+            return null;
+        }
+
+        private static Tuple<string, string> DetectMagic(uint magic)
+        {
+            switch (magic)
+            {
+                case 0x5374726D: /* 'Strm'    */ return new Tuple<string, string>("strm", "bin");
+                case 0x00584254: /* '\0XBT'   */ return new Tuple<string, string>("gfx", "xbt");
+                case 0x4D455348: /* 'MESH'    */ return new Tuple<string, string>("gfx", "xbg");
+                case 0x54414D00: /* '\0MAT'   */ return new Tuple<string, string>("gfx", "material.bin");
+                case 0x53504B02: /* 'SPK\2'   */ return new Tuple<string, string>("sfx", "spk");
+                case 0x4643626E: /* 'FCbn'    */ return new Tuple<string, string>("game", "fcb");
+                case 0x534E644E: /* 'SNdN'    */ return new Tuple<string, string>("game", "rnv");
+                case 0x474E5089: /* 'PNG\x89' */ return new Tuple<string, string>("gfx", "png");
+                case 0x4D564D00: /* 'MVM\0'   */ return new Tuple<string, string>("gfx", "MvN");
+                case 0x61754C1B: /* 'Lua\x1B' */ return new Tuple<string, string>("scripts", "luab");
+                case 0x47454F4D: /* 'GEOM'    */ return new Tuple<string, string>("gfx", "xbg");
+                case 0x42544348: /* 'BTCH'    */ return new Tuple<string, string>("cbatch", "cbatch");
+                case 0x53524852: /* 'SRHR'    */ return new Tuple<string, string>("srhr", "bin");
+                case 0x53524C52: /* 'SRLR'    */ return new Tuple<string, string>("srlr", "bin");
+                case 0x53435452: /* 'SCTR'    */ return new Tuple<string, string>("sctr", "bin");
+                case 0x54524545: /* 'TREE'    */ return new Tuple<string, string>("tree", "bin");
+                case 0x50494D47: /* 'PIMG'    */ return new Tuple<string, string>("pimg", "bin");
+                case 0x45534142: /* 'BASE'    */ return new Tuple<string, string>("wlu", "fcb");
+                case 0x66314130: /* 'f1A0'    */ return new Tuple<string, string>("dialog", "stimuli.dsc.pack");
+                case 0x6732424B: /* 'g2BK'    */ return new Tuple<string, string>("bink", "bik");
+                case 0x0A6F6E61: /* 'ano\n'   */ return new Tuple<string, string>("annotation", "ano");
+                case 0x4C695072: /* 'LiPr'    */ return new Tuple<string, string>("lightprobe", "lipr.bin");
+                case 0x4D763211: /* 'Mv2\x11' */ return new Tuple<string, string>("move", "bin");
+                case 0x534C4852: /* 'SLHR'    */ return new Tuple<string, string>("roadresources", "hgfx");
+                case 0x474D4950: /* 'GMIP'    */ return new Tuple<string, string>("gfx", "xbgmip");
+                case 0x4C504D54: /* 'LPMT'    */ return new Tuple<string, string>("bin", "lpmt");
+                case 0x424B4844: /* 'BKHD'    */ return new Tuple<string, string>("bin", "bkhd");
+                case 0x434B5441: /* 'CKTA'    */ return new Tuple<string, string>("bin", "ckta");
+                case 0x4F54544F: /* 'OTTO'    */ return new Tuple<string, string>("bin", "otto");
+                case 0x4D475246: /* 'MGRF'    */ return new Tuple<string, string>("bin", "mgrf");
+                case 0x00014C53: /*           */ return new Tuple<string, string>("languages", "loc");
+                case 0x00032A02: /*           */ return new Tuple<string, string>("sfx", "sbao");
+                case 0x0000389C: /*           */ return new Tuple<string, string>("eight", "bin");
+
+            }
             return null;
         }
     }
